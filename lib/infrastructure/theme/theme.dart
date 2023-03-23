4aebe70d7e015b14fa0b62d3884c1792ab49cee2
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-03-23 17:29:03 +0800

    update

--- lib/infrastructure/theme/theme.dart
+++ lib/infrastructure/theme/theme.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const Color blue = Color(0xFF98D4FF);
-const Color blue2 = Color(0xFF20A1FF);
-const Color blackComponent = Color(0xFF1E1E1E);
-const Color grey1 = Color(0xFF333333);
+const Color grey1 = Color(0xFF828282);
 const Color grey2 = Color(0xFF7584A2);
-const Color grey3 = Color(0xFF828282);
 const Color white = Color(0xFFFFFFFF);
-const Color blackColor = Color(0xFF000000);
+const Color blueDark = Color(0xFF14193F);
 const Color lightGrey = Color(0xFFE8F0FA);
 const Color red = Color(0xFFCB0000);
 
-Color kWhiteColor = Color(0xffFFFFFF);
-
-TextStyle blackTextStyle = GoogleFonts.poppins(
-  color: blackColor,
-);
-TextStyle whiteTextStyle = GoogleFonts.poppins(
-  color: kWhiteColor,
-);
-
-TextStyle blueTextStyle2 = GoogleFonts.poppins(
-  color: blue2,
+TextStyle blueDarkkTextStyle = GoogleFonts.poppins(
+  color: blueDark,
 );
 
 TextStyle greyTextStyle = GoogleFonts.poppins(
