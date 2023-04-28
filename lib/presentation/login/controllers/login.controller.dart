09783ab5516abb7318d5866aa2da5bad99b9591c
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-04-28 11:26:28 +0800

    add login function

--- lib/presentation/login/controllers/login.controller.dart
+++ lib/presentation/login/controllers/login.controller.dart
@@ -1,7 +1,98 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../infrastructure/theme/theme.dart';
+
 class LoginController extends GetxController {
-  TextEditingController emailC = TextEditingController();
-  TextEditingController passC = TextEditingController();
+  final GlobalKey<FormState> formKe1 = GlobalKey<FormState>();
+  TextEditingController emailC = TextEditingController(text: 'admin@gmail.com');
+  TextEditingController passC = TextEditingController(text: 'admin12345');
+
+  void login() async {
+    // Pastikan ada koneksi internet
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      infoFailed(
+          "Tidak ada Internet", "Silahkan periksa koneksi internet Anda");
+      return;
+    }
+  }
+
+  void infoFailed(String msg1, String msg2) {
+    Get.snackbar(
+      "",
+      "",
+      backgroundColor: oldRose,
+      colorText: white,
+      snackPosition: SnackPosition.BOTTOM,
+      margin: const EdgeInsets.all(0),
+      borderRadius: 0,
+      duration: const Duration(seconds: 4),
+      animationDuration: const Duration(milliseconds: 600),
+      reverseAnimationCurve: Curves.easeInBack,
+      isDismissible: true,
+      dismissDirection: DismissDirection.horizontal,
+      showProgressIndicator: false,
+      overlayBlur: 0.0,
+      overlayColor: darkBlue,
+      icon: const Icon(
+        EvaIcons.alertCircleOutline,
+        color: white,
+      ),
+      shouldIconPulse: true,
+      leftBarIndicatorColor: oldRose,
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      snackStyle: SnackStyle.FLOATING,
+      titleText: Text(
+        msg1,
+        style: whiteTextStyle.copyWith(
+          fontWeight: bold,
+        ),
+      ),
+      messageText: Text(
+        msg2,
+        style: whiteTextStyle.copyWith(fontWeight: medium, fontSize: 12),
+      ),
+    );
+  }
+
+  void infoSuccess(String msg1, String msg2) {
+    Get.snackbar(
+      "",
+      "",
+      backgroundColor: shamrockGreen,
+      colorText: white,
+      snackPosition: SnackPosition.BOTTOM,
+      margin: const EdgeInsets.all(0),
+      borderRadius: 0,
+      duration: const Duration(seconds: 4),
+      animationDuration: const Duration(milliseconds: 600),
+      reverseAnimationCurve: Curves.easeInBack,
+      isDismissible: true,
+      dismissDirection: DismissDirection.horizontal,
+      showProgressIndicator: false,
+      overlayBlur: 0.0,
+      overlayColor: white, // Menggunakan warna putih
+      icon: const Icon(
+        EvaIcons.checkmarkCircle2Outline,
+        color: white,
+      ),
+      shouldIconPulse: true,
+      leftBarIndicatorColor: shamrockGreen,
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      snackStyle: SnackStyle.FLOATING,
+      titleText: Text(
+        msg1,
+        style: whiteTextStyle.copyWith(
+          fontWeight: bold,
+        ),
+      ),
+      messageText: Text(
+        msg2,
+        style: whiteTextStyle.copyWith(fontWeight: medium, fontSize: 12),
+      ),
+    );
+  }
 }
